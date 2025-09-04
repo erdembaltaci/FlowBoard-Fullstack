0ab64f0a67b1953c87ef848d8cee1db1d197e5c1
ALİ ERDEM BALTACI <103959698+erdembaltaci@users.noreply.github.com>
2025-09-04 21:37:42 +0300

    Update IssueListPage.jsx

--- Frontend/src/pages/IssueListPage.jsx
+++ Frontend/src/pages/IssueListPage.jsx
@@ -114,7 +114,7 @@ function IssueListPage() {
                             className="flex items-center gap-2 rounded-full bg-transparent border-slate-600 text-slate-300 hover:bg-slate-700 hover:text-white"
                             title={`${m.fullName} kullanıcısının görev panosuna git`}
                         >
-                            <Avatar className="h-6 w-6"><AvatarImage src={fileUrl(member.avatarUrl)} alt={member.fullName} /><AvatarFallback>{m.fullName?.charAt(0)}</AvatarFallback></Avatar>
+                            <Avatar className="h-6 w-6"><AvatarImage src={fileUrl(user.avatarUrl)} alt={user.fullName} /><AvatarFallback>{m.fullName?.charAt(0)}</AvatarFallback></Avatar>
                             {m.fullName || m.email}
                         </Button>
                     ))}
