3f1bdcbcbf9e7317994a3f8ed6daf734ff99a82a
ALİ ERDEM BALTACI <103959698+erdembaltaci@users.noreply.github.com>
2025-09-04 17:27:05 +0300

    Update UserDto.cs

--- JiraProject.Business/Dtos/UserDto.cs
+++ JiraProject.Business/Dtos/UserDto.cs
@@ -8,5 +8,7 @@ namespace JiraProject.Business.Dtos
         public string Email { get; set; } = null!;
         public string Role { get; set; } = null!; 
         public DateTime CreatedAt { get; set; }
+        public string? AvatarUrl { get; set; }
+
     }
 }
