b8e64ecbcccb0f290aa4a32b223f1c41211bd1d1
ALİ ERDEM BALTACI <103959698+erdembaltaci@users.noreply.github.com>
2025-09-06 21:00:35 +0300

    Update Program.cs

--- JiraProject.WebAPI/Program.cs
+++ JiraProject.WebAPI/Program.cs
@@ -15,6 +15,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
 using System.Text.Json.Serialization;
+using CloudinarySettings.cs;
 
 var builder = WebApplication.CreateBuilder(args);
 
