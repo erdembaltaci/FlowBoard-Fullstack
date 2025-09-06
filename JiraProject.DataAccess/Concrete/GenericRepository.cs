507a13ca5b294346ff356af9cb72af97ea0b0db3
ALİ ERDEM BALTACI <103959698+erdembaltaci@users.noreply.github.com>
2025-09-06 19:35:21 +0300

    Update GenericRepository.cs

--- JiraProject.DataAccess/Concrete/GenericRepository.cs
+++ JiraProject.DataAccess/Concrete/GenericRepository.cs
@@ -51,6 +51,7 @@ public class GenericRepository<T> : IGenericRepository<T> where T : class
     public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
     public void Update(T entity) => _dbSet.Update(entity);
     public void Delete(T entity) => _dbSet.Remove(entity);
+    public void Remove(T entity) => _dbSet.Remove(entity);
     public IQueryable<T> GetQueryableWithIncludes(params string[] includeStrings)
     {
         IQueryable<T> query = _dbSet;
