4a6cbf9cee25841184b172c0ab1d1b2f9bfe4c1c
ALİ ERDEM BALTACI <103959698+erdembaltaci@users.noreply.github.com>
2025-09-07 03:00:22 +0300

    Update UserManager.cs

--- JiraProject.Business/Concrete/UserManager.cs
+++ JiraProject.Business/Concrete/UserManager.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace JiraProject.Business.Concrete
 {
