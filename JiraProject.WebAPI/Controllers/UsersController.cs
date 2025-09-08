25c353912ad1c75b67ccde08ee48584b464d9d15
ALİ ERDEM BALTACI <103959698+erdembaltaci@users.noreply.github.com>
2025-09-08 20:51:02 +0300

    Update UsersController.cs

--- JiraProject.WebAPI/Controllers/UsersController.cs
+++ JiraProject.WebAPI/Controllers/UsersController.cs
@@ -82,11 +82,7 @@ namespace JiraProject.WebAPI.Controllers
 
             return Ok(new { message = "Kullanıcı rolü başarıyla değiştirildi." });
         }
-
-        // ---------------------------------------------------------------------
-        // DİĞER METOTLAR (Eğer bir admin paneli yapmayacaksanız gereksiz olabilir)
-        // ---------------------------------------------------------------------
-
+        
         /// <summary>
         /// Tüm kullanıcıları listeler.
         /// </summary>
